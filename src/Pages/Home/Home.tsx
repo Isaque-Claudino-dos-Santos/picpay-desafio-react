860c5803afc89f2439bb28444aba9d307dbf81ff
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-11-01 17:38:58 -0300

    use title

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import CurrentPageTextRibbon from '../../Components/CurrentPageTextRibbon';
+import Title from '../../Components/Title';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <header>
         <CurrentPageTextRibbon text="Home" />
-        {/* title */}
+        <Title text="Welcome to home page" />
         {/* nav bar */}
       </header>
     </>
